b2f60a523c8b76757ce9f4184a0fbe6b6118e4a0
lx5321 <alexanderwebsite5321@gmail.com>
2020-01-30 18:28:17 +0530

    MLP Classifier

--- OnlyMLP.py
+++ OnlyMLP.py
@@ -1,24 +1,3 @@
-#!/usr/bin/env python
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import StandardScaler
 
-diabetes = pd.read_csv('diabetes.csv')
-X_train, X_test, y_train, y_test = train_test_split(diabetes.loc[:, diabetes.columns != 'Outcome'], diabetes['Outcome'], stratify=diabetes['Outcome'], random_state=66)
 
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.fit_transform(X_test)
-
-mlp = MLPClassifier(random_state=42)
-mlp.fit(X_train_scaled, y_train)
-
-print("Accuracy on training set: {:.3f}".format(mlp.score(X_train_scaled, y_train)))
-print("Accuracy on test set: {:.3f}".format(mlp.score(X_test_scaled, y_test)))
-
-sol = (mlp.predict([[3,185,36,239,1,236.6,0.701,31]]))
-sol = float(sol)
-print(sol)
 
