7f8bc3d4dac1c5e14d542b57ad51349ce2509025
lx5321 <alexanderwebsite5321@gmail.com>
2020-01-30 18:41:20 +0530

    Added MLP to slave

--- slave.py
+++ slave.py
@@ -5,48 +5,54 @@ from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from sys import argv as a
-from time import sleep as s
 import env_vars as en
-import itertools
 import mysql.connector
 
-temp = (a[1])
-temp = temp.split(',')
+import warnings
+warnings.filterwarnings("ignore")
+
 
 try:
     mydb = mysql.connector.connect(host=en.host, user=en.user, passwd=en.passwd, database=en.database)
     mycursor = mydb.cursor()
 except Error as e:
-        print(e)
-        exit(0)
+    print(e)
+    exit(0)
 
 def db_connect(query_id):
-        pending = []
-        query = "select * from {} where id={}".format("diagnosis", query_id)
-        mycursor.execute(query)
-        myresult = mycursor.fetchall()
-        for x in myresult:
-            print(x)
-
-        
-diabetes = pd.read_csv('diabetes.csv')
-X_train, X_test, y_train, y_test = train_test_split(diabetes.loc[:, diabetes.columns != 'Outcome'], diabetes['Outcome'], stratify=diabetes['Outcome'], random_state=66)
-
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.fit_transform(X_test)
-
-mlp = MLPClassifier(random_state=42)
-mlp.fit(X_train_scaled, y_train)
-
-print("Accuracy on training set: {:.3f}".format(mlp.score(X_train_scaled, y_train)))
-print("Accuracy on test set: {:.3f}".format(mlp.score(X_test_scaled, y_test)))
-
-sol = (mlp.predict([[3,185,36,239,1,236.6,0.701,31]]))
-sol = float(sol)
-print(sol)
+    pending = []
+    query = "select * from {} where id={}".format("diagnosis", query_id)
+    mycursor.execute(query)
+    myresult = mycursor.fetchall()
+    # for x in myresult:
+    #     print(x)
+    
+    sol = (mlp.predict([[3,185,36,239,1,236.6,0.701,31]]))
+    sol = float(sol)
+    print(sol)
    
 
 if __name__ == "__main__":
+    # split array and store it in temp
+    temp = (a[1])
+    temp = temp.split(',')
+
+    diabetes = pd.read_csv('diabetes.csv')
+
+
+    X_train, X_test, y_train, y_test = train_test_split(diabetes.loc[:, diabetes.columns != 'Outcome'], diabetes['Outcome'], stratify=diabetes['Outcome'], random_state=66)
+
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.fit_transform(X_test)
+
+    global mlp
+    mlp = MLPClassifier(random_state=42)
+    mlp.fit(X_train_scaled, y_train)
+
+    print("Accuracy on training set: {:.3f}".format(mlp.score(X_train_scaled, y_train)))
+    print("Accuracy on test set: {:.3f}".format(mlp.score(X_test_scaled, y_test)))
+
+
     for queries in temp:
 	    db_connect(queries)
